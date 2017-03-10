94529a9c532883ff4f1aa1fe1466f6d6f26d4074
Umesh Jakhar <jakharumesh3@gmail.com>
2017-03-10 16:31:04 +0530

    First commit

---
+++ app/src/main/java/com/datababa/app/MainActivity.java
@@ -0,0 +1,388 @@
+package com.datababa.app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.*;
+import com.google.example.games.basegameutils.BaseGameUtils;
+
+public class MainActivity extends AppCompatActivity {
+    private String postUrl = "https://google.co.in";
+    private WebView webView;
+    private ProgressBar progressBar;
+    private float m_downX;
+    private ImageView imgHeader;
+
+    public String TAG = "playgames";
+
+    private Button mainButton;
+    private TextView scoreView;
+    private TextView timeView;
+
+    private int score = 0;
+    private boolean playing = false;
+    private GoogleApiClient apiClient;
+    private  Boolean isConnected = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        Log.i("is connected   ", "true nahi hua");
+
+        apiClient = new GoogleApiClient.Builder(this)
+                .addApi(Games.API)
+                .addScope(Games.SCOPE_GAMES)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Log.e(TAG, "Could not connect to Play games services");
+                        // finish();
+
+                    }
+                }).build();
+
+        apiClient.connect();
+
+
+        if (apiClient != null && apiClient.isConnected())
+        {
+            isConnected=true;
+            Log.i("isconnedted", "true ho gya");
+        }else {
+            apiClient.connect();
+            Log.i("is connected   ", "true nahi hua");
+        }
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+
+        mainButton = (Button)findViewById(R.id.main_button);
+        scoreView = (TextView)findViewById(R.id.score_view);
+        timeView = (TextView)findViewById(R.id.time_view);
+       // toolbar.setVisibility(View.GONE);
+
+
+
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!playing) {
+                    // The first click
+                    playing = true;
+                    mainButton.setText("Keep Browsing");
+
+                    // Initialize CountDownTimer to 60 seconds
+                    new CountDownTimer(60000, 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            timeView.setText("Time remaining: " + millisUntilFinished/1000);
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            playing = false;
+                            timeView.setText("Session over");
+                            mainButton.setVisibility(View.GONE);
+                        }
+                    }.start();  // Start the timer
+                } else {
+                    // Subsequent clicks
+                    score++;
+                    Games.Leaderboards.submitScore(apiClient,
+                            getString(R.string.leaderboard_datababa_leaderboard),
+                            score);
+                    scoreView.setText("Score: " + score + " points");
+                    if(score>100) {
+                        Games.Achievements
+                                .unlock(apiClient,
+                                        getString(R.string.achievement_bronze_medal));
+                    }
+                }
+            }
+        });
+
+
+        webView = (WebView) findViewById(R.id.webView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        imgHeader = (ImageView) findViewById(R.id.backdrop);
+        //imgHeader.setVisibility(View.GONE);
+
+        if (!TextUtils.isEmpty(getIntent().getStringExtra("postUrl"))) {
+            postUrl = getIntent().getStringExtra("postUrl");
+        }
+
+        initWebView();
+        initCollapsingToolbar();
+        renderPost();
+
+
+        // enable / disable javascript
+        // webView.getSettings().setJavaScriptEnabled(true);
+
+        // loading url into web view
+        // webView.loadUrl("http://www.google.com");
+
+        /**
+         * loading custom html into webivew
+         * */
+        /*
+        String customHtml = "<html><body><h1>Hello, WebView</h1> <h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3>" +
+                "<p>This is a sample paragraph.</p></body></html>";
+        webView.loadData(customHtml, "text/html", "UTF-8");
+        */
+
+        /**
+         * Enabling zoom-in controls
+         * */
+        /*
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(true);
+        */
+
+        // Loading local html file into web view
+        // webView.loadUrl("file:///android_asset/sample.html");
+
+        /**
+         * Loading custom fonts and css
+         * */
+        /*
+        String style = "<style type='text/css'>@font-face { font-family: 'roboto'; src: url('Roboto-Light.ttf');}@font-face { font-family: 'roboto-medium'; src: url('Roboto-Medium.ttf'); }" +
+                "body{color:#666;font-family: 'roboto';padding: 0.3em;}";
+        style += "a{color:" + String.format("#%06X", (0xFFFFFF & ContextCompat.getColor(this, R.color.colorPrimaryDark))) + "}</style>";
+        String customHtml = "<h1>Hello, WebView</h1> <h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3>" +
+                "<p>This is a sample paragraph.</p>";
+        String content = "<html>" + style + "<body'>" + customHtml + "</body></Html>";
+        webView.loadDataWithBaseURL("file:///android_asset/", content, "text/html", "utf-8", null);
+        */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+   /*
+        if (Utils.isBookmarked(this, webView.getUrl())) {
+            // change icon color
+            Utils.tintMenuIcon(getApplicationContext(), menu.getItem(0), R.color.colorAccent);
+        } else {
+            Utils.tintMenuIcon(getApplicationContext(), menu.getItem(0), android.R.color.white);
+        }
+        */
+        return true;
+    }
+
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        // menu item 0-index is bookmark icon
+
+        // enable - disable the toolbar navigation icons
+        if (isConnected == true) {
+            menu.getItem(R.id.achievements_button).setEnabled(true);
+            menu.getItem(R.id.achievements_button).getIcon().setAlpha(255);
+            menu.getItem(1).setEnabled(true);
+            menu.getItem(1).getIcon().setAlpha(255);
+        } else {
+            //menu.getItem(0).setEnabled(false);
+            menu.getItem(0).getIcon().setAlpha(130);
+            //menu.getItem(1).setEnabled(false);
+            menu.getItem(1).getIcon().setAlpha(130);
+        }
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.achievements_button) {
+            showAchievements();
+        }
+        if (item.getItemId() == R.id.leaderboard_button) {
+            showLeaderboard();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void showLeaderboard() {
+        startActivityForResult(
+                Games.Leaderboards.getLeaderboardIntent(apiClient,
+                        getString(R.string.leaderboard_datababa_leaderboard)), 0);
+    }
+
+    public void showAchievements() {
+        startActivityForResult(
+                Games.Achievements
+                        .getAchievementsIntent(apiClient),
+                1
+        );
+    }
+
+    private void initWebView() {
+        webView.setWebChromeClient(new MyWebChromeClient(this));
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                /**
+                 * Check for the url, if the url is from same domain
+                 * open the url in the same activity as new intent
+                 * else pass the url to browser activity
+                 * */
+                if (Utils.isSameDomain(postUrl, url)) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    intent.putExtra("postUrl", url);
+                    startActivity(intent);
+                } else {
+                    // launch in-app browser i.e BrowserActivity
+                    openInAppBrowser(url);
+                }
+
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+        webView.clearCache(true);
+        webView.clearHistory();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getPointerCount() > 1) {
+                    //Multi touch detected
+                    return true;
+                }
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        // save the x
+                        m_downX = event.getX();
+                    }
+                    break;
+
+                    case MotionEvent.ACTION_MOVE:
+                    case MotionEvent.ACTION_CANCEL:
+                    case MotionEvent.ACTION_UP: {
+                        // set x so that it doesn't move
+                        event.setLocation(m_downX, event.getY());
+                    }
+                    break;
+
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void renderPost() {
+        webView.loadUrl(postUrl);
+
+        // webView.loadUrl("file:///android_asset/sample.html");
+    }
+
+    private void openInAppBrowser(String url) {
+        Intent intent = new Intent(MainActivity.this, BrowserActivity.class);
+        intent.putExtra("url", url);
+        startActivity(intent);
+    }
+
+    /**
+     * Initializing collapsing toolbar
+     * Will show and hide the toolbar txtPostTitle on scroll
+     */
+    private void initCollapsingToolbar() {
+        final CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(" ");
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        appBarLayout.setExpanded(true);
+
+        // hiding & showing the txtPostTitle when toolbar expanded & collapsed
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbar.setTitle("Web View");
+                    isShow = true;
+                } else if (isShow) {
+                    collapsingToolbar.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
+
+        // loading toolbar header image
+        Glide.with(getApplicationContext()).load("http://api.androidhive.info/webview/nougat.jpg")
+                .thumbnail(0.5f)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imgHeader);
+    }
+
+    private class MyWebChromeClient extends WebChromeClient {
+        Context context;
+
+        public MyWebChromeClient(Context context) {
+            super();
+            this.context = context;
+        }
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+
+}
